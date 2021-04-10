d6cea57646d1ec74ff51fa51e6328bbd1034b928
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-04-10 05:00:29 -0300

    Array of suggestive words inserted

--- LCSLearnedCodeSheetJS.js
+++ LCSLearnedCodeSheetJS.js
@@ -1862,6 +1862,23 @@ const displaySuggestions = (event) => {
 
 submit.addEventListener('click', displaySuggestions);
 
+// To receive an array of suggestive words =
+const getSuggestions = () => {
+    const wordQuery = inputField.value;
+    const endpoint = `${url}${queryParams}${wordQuery}`;
+    
+    fetch(endpoint, {cache: 'no-cache'}).then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Request failed!');
+    }, networkError => {
+      console.log(networkError.message)
+    }).then(jsonResponse => {
+      renderResponse(jsonResponse);
+    })
+  }
+
 
 
 
