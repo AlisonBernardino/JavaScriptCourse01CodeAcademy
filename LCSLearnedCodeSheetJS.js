6eba3db42eb8b125a219c5aa4f5c45e87688e23c
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-04-10 04:28:51 -0300

    GET request III added

--- LCSLearnedCodeSheetJS.js
+++ LCSLearnedCodeSheetJS.js
@@ -1825,6 +1825,43 @@ fetch('http://api-to-call.com/endpoint').then(response => {
     });
 
 
+// Code 56 - fetch GET request III
+
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+
+// Select page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+    const wordQuery = inputField.value;
+    const endpoint = `${url}${queryParams}${wordQuery}`;
+    fetch(endpoint,{cache: 'no-cache'}).then(response => {
+        if(response.ok){
+            // renderJsonResponse(response);
+            return response.json();
+        }
+        throw new Error('Request failed!');
+    }, networkError => {
+        console.log(networkError.message)
+    })
+}
+
+// Clears previous results and display results to
+// webpage
+const displaySuggestions = (event) => {
+    event.preventDefault();
+    while(responseField.firstChild){
+        responseField.removeChild(responseField.firstChild);
+    }
+    getSuggestions();
+};
+
+submit.addEventListener('click', displaySuggestions);
+
 
 
 
