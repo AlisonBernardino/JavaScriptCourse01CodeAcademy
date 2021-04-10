f79c34cc546c817cb4b495dc9c7c96e802f7fcf0
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-04-10 20:45:01 -0300

    Update test to correct changes

--- LCSLearnedCodeSheetJS.js
+++ LCSLearnedCodeSheetJS.js
@@ -1,4 +1,4 @@
-Advanced objects = 02 / 06 / 2021 - 05: 02 AM
+// Advanced objects = 02 / 06 / 2021 - 05: 02 AM
 
 // Code 01 = Calling object attributes using "this." method
 
@@ -653,78 +653,78 @@ var carbonara = "You can make carbonara with "
 // Code 20 - Commands to install Babel, 
 // the JavaScript ES6 to ES5 converter
 
-npm install babel - cli
-npm install babel - preset - env
+// npm install babel - cli
+// npm install babel - preset - env
 
 // To see the ES5 code in ./lib/main.js
-npm run build
+// npm run build
 
 // To see the file structure inside my current directory
-ls
+// ls
 
-// To create a new package.json
-npm init
+// // To create a new package.json
+// npm init
 
 
-// Code 21 - Install Node packages
+// // Code 21 - Install Node packages
 
-We use the npm install command to
-install new Node packages locally.
-The install command creates a folder called
-node_modules and copies the package files to it.
-The install command also installs all
-of the dependencies for the given package.
+// // We use the npm install command to
+// install new Node packages locally.
+// The install command creates a folder called
+// node_modules and copies the package files to it.
+// The install command also installs all
+// of the dependencies for the given package.
 
-To install Babel, we need to npm install
-two packages, babel - cli and babel - preset - env.
-    However, npm installs over one hundred other
-packages that are dependencies for Babel to 
- run properly.
+// To install Babel, we need to npm install
+// two packages, babel - cli and babel - preset - env.
+//     However, npm installs over one hundred other
+// packages that are dependencies for Babel to 
+//  run properly.
 
-We install Babel with the following two commands:
+// We install Babel with the following two commands:
 
-$ npm install babel - cli - D
-$ npm install babel - preset - env - D
-The babel - cli package includes command line Babel
-tools, and the babel - preset - env package has the code
-that maps any JavaScript feature, ES6 and above(ES6 +),
-    to ES5.
+// $ npm install babel - cli - D
+// $ npm install babel - preset - env - D
+// The babel - cli package includes command line Babel
+// tools, and the babel - preset - env package has the code
+// that maps any JavaScript feature, ES6 and above(ES6 +),
+//     to ES5.
 
-        The - D flag instructs npm to add each package
-to a property called devDependencies in package.json.
-Once the project’s dependencies are listed in
-    devDependencies, other developers can run your project
-without installing each package separately.Instead,
-    they can simply run npm install — it instructs npm
-to look inside package.json and download all of the
-packages listed in devDependencies.
+//         The - D flag instructs npm to add each package
+// to a property called devDependencies in package.json.
+// Once the project’s dependencies are listed in
+//     devDependencies, other developers can run your project
+// without installing each package separately.Instead,
+//     they can simply run npm install — it instructs npm
+// to look inside package.json and download all of the
+// packages listed in devDependencies.
 
-Once you npm install packages, you can find the
-Babel packages and all their dependencies in the
-node_modules folder.The new directory structure
-contains the following:
+// Once you npm install packages, you can find the
+// Babel packages and all their dependencies in the
+// node_modules folder.The new directory structure
+// contains the following:
 
-project
-    | _ node_modules
-        | ___.bin
-        | ___ ...
-| _ src
-    | ___ main.js
-        | _ package.json
-The ... in the file structure above is a placeholder
-for 100 + packages that npm installed.
+// project
+//     | _ node_modules
+//         | ___.bin
+//         | ___ ...
+// | _ src
+//     | ___ main.js
+//         | _ package.json
+// The ... in the file structure above is a placeholder
+// for 100 + packages that npm installed.
 
 // Use npm with the -D flag to install the Babel 
 // command line package and add it to the devDependencies
 // property in package.json.
 // There is no space between the "-" and the commands
-npm install babel - cli - D
+// npm install babel - cli - D
 
 // Use npm with the -D flag to install the Babel 
 // preset environment package and add it to the 
 // devDependencies property in package.json.
 // There is no space between the "-" and the commands
-npm install babel - preset - env
+// npm install babel - preset - env
 
 // Enter ls into the terminal to see the file 
 // structure of your current directory. Notice
@@ -735,14 +735,14 @@ npm install babel - preset - env
 
 
 // To create a new .babelrc file
-touch.babelrc
+// touch.babelrc
 
 // To add a script called "build" in package.json file:
-{
-    scripts {
-        "build": "babel src -d lib"
-    }
-}
+// {
+//     scripts {
+//         "build": "babel src -d lib"
+//     }
+// }
 
 // Code 22 - module.exports
 
