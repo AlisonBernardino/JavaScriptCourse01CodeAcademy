60ad131607ea2702add5a825e3db517e4de6d61e
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-04-15 00:21:02 -0300

    async POST request II added

--- LCSLearnedCodeSheetJS.js
+++ LCSLearnedCodeSheetJS.js
@@ -2077,4 +2077,23 @@ const displaySuggestions = (event) => {
 submit.addEventListener('click',displaySuggestions);
 
 
+// Code 64 - async POST requests II
+
+const getData = async () => {
+    try{
+        const response = await fetch('https://api-to-call.com/endpoint', {
+            method: 'POST',
+            body: JSON.stringify({id: 200});
+        })
+        if(response.ok){
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        }
+        throw new Error('Request failed!');
+        } catch(error){
+            console.log(error);
+        }
+}
+
+
 
