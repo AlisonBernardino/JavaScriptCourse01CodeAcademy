5bb23a6ad043c0a879a33f6cf6abc50608d6dfde
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-04-15 02:07:22 -0300

    final code - Requests II added

--- LCSLearnedCodeSheetJS.js
+++ LCSLearnedCodeSheetJS.js
@@ -2096,4 +2096,87 @@ const getData = async () => {
 }
 
 
+// Final code - Requests II
 
+// NOTE: wordSmith functions from lines 4-39
+// NOTE02: byteSize functions from lines 48-81
+// (Remember to add my API key)
+
+// Info to reach API
+const dataMuseUrl = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jjb=';
+
+// Selecting the page elements
+const inputFueld = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = async () => {
+    const wordQuery = inputField.value;
+    const endpoint = dataMuseUrl + queryParams + wordQuery;
+
+    try{
+        const response = await fetch(endpoint);
+        if(response.ok){
+            let josnResponse = await response.json();
+            renderWordResponse(jsonResponse);
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+    event.preventDefault();
+    while(responseField.firstChild){
+        responseField.removeChild(responseField.firstChild);
+    }
+    getSuggestions();
+}
+
+submit.addEventListener('click',displaySuggestions);
+
+// Info to reach Rebrandly API
+const apiKey = '<My API key>';
+const rebrandlyEndpoint = 'https://api.rebrandly.com/v1/links';
+
+// Element selector
+const shortenButton = document.querySelector('#shorten');
+
+// AJAX functions
+const shortenUrl = async () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({destination: urlToShorten});
+
+    try{
+        const response = await fetch(rebrandlyEndpoint, {
+            method: 'POST',
+            body: data,
+            headers: {
+                "Content-type": "application/json",
+                'apikey': apiKey
+            }
+        })
+        if(response.ok){
+            const jsonResponse = await response.json();
+            renderByteResponse(jsonResponse);
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+    event.preventDefault();
+    while(responseField.firstChild){
+        responseField.removeChild(responseField.firstChild);
+    }
+    shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
